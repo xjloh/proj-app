fe68e7f10df7d377c63dd25ff6d0f2d9f2a8d070
jamie <xinjun@peachpay.app>
2025-01-12 15:17:04 +0800

    use sanity to generate types and completed startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,12 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
   const query = (await searchParams).query;
 
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: { _id: 1, name: 'Loh' },
-    _id: 1,
-    description: 'This is a description.',
-    image: '',
-    category: 'Robots',
-    title: 'We Robots'
-  }];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
@@ -30,7 +23,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartUpCardType, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post}/>
             ))
           ) : (
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
     const {
@@ -40,17 +43,17 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                         <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
                     </Link>
                 </div>
-                <Link href={`/user/${author._id}`}>
-                    <Image src="" alt='placeholder' width={48} height={48} className='rounded-full'></Image>
+                <Link href={`/user/${author?._id}`}>
+                    <Image src={image} alt='placeholder' width={48} height={48} className='rounded-full'></Image>
                 </Link>
             </div>
 
             <Link href={`/startup/${_id}`}>
                 <p className='startup-card_desc'>{description}</p>
-                <Image src={image} alt='placeholder' className='startup-card_img'></Image>
+                <img src={image} alt='placeholder' className='startup-card_img'></img>
             </Link>
-            <div className='flex-between gap-3 mt5'>
-                <Link href={`/?query=${category.toLowerCase()}`}>
+            <div className='flex-between gap-3 mt-5'>
+                <Link href={`/?query=${category?.toLowerCase()}`}>
                     <p className='text-16-medium'>{category}</p>
                 </Link>
                 <Button className='startup-card_btn' asChild>
