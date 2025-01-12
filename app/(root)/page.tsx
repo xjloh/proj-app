8a67696218dc1e3ffdbb88fb0df80e91d190af12
jamie <xinjun@peachpay.app>
2025-01-12 15:38:26 +0800

    fixed search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,8 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
   const query = (await searchParams).query;
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
