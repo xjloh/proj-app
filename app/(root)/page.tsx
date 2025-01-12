3ebcc1c199ab6b1e8078be37efc6f39cea3bc98d
jamie <xinjun@peachpay.app>
2025-01-12 15:31:29 +0800

    implemented sanity live

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -31,6 +30,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+      <SanityLive></SanityLive>
     </>
   );
 }
