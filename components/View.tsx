1ab1e3e5b281ce73bc0983e54185a50e26bb992d
jamie <xinjun@peachpay.app>
2025-01-12 17:46:07 +0800

    completed startup details page, added nextjs ppr

---
+++ components/View.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Ping from './Ping'
+import { client } from '@/sanity/lib/client'
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+
+const View = async ({ id }: { id: string }) => {
+    const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id });
+    
+    // TODO: Update the number of views
+    return (
+        <div className='view-container'>
+            <div className='absolute -top-2 -right-2'>
+                <Ping></Ping>
+            </div>
+            <p className='view-text'>
+                {/* TODO: Views or view */}
+                <span className='font-black'>Views: {totalViews}</span>
+            </p>
+      </div>
+  )
+}
+
+export default View
--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    ppr: 'incremental'
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
+  }
 };
 
 export default nextConfig;
