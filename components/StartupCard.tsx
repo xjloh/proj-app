a8dfee94a3d8b4bcd3d3698f292dbb47c596a63a
jamie <xinjun@peachpay.app>
2025-01-13 23:52:19 +0800

    completed user profile page

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,11 @@
-import { formatDate } from '@/lib/utils'
+import { cn, formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { Author, Startup } from '@/sanity/types'
+import { Skeleton } from './ui/skeleton'
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -44,7 +45,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                     </Link>
                 </div>
                 <Link href={`/user/${author?._id}`}>
-                    <Image src={image} alt='placeholder' width={48} height={48} className='rounded-full'></Image>
+                    {/* TODO: this needs to be modified */}
+                    <Image src={author?.image!} alt={author?.name!} width={48} height={48} className='rounded-full'></Image>
                 </Link>
             </div>
 
@@ -63,5 +65,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
   )
 }
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+        <li key={cn("skeleton", index)}>
+            <Skeleton className="startup-card_skeleton" />
+        </li>
+        ))}
+    </>
+);
 
 export default StartupCard
