2569e652775979a32132f8ded13b9dfd2cd84a6e
jamie <xinjun@peachpay.app>
2025-01-14 00:04:57 +0800

    modified nav bar for mobile view

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
 import { auth, signOut, signIn } from "@/auth.ts"
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
     const session = await auth();
@@ -17,8 +19,9 @@ const Navbar = async () => {
                 <div className='flex items-center gap-5 text-black'>
                     {session && session?.user ? (
                         <>
-                            <Link href='/startup/create'>
-                                <span>Create</span>
+                            <Link href="/startup/create">
+                                <span className="max-sm:hidden">Create</span>
+                                <BadgePlus className="size-6 sm:hidden" />
                             </Link>
 
                             <form
@@ -29,11 +32,18 @@ const Navbar = async () => {
                             >
                                 <button type="submit">
                                     <span className="max-sm:hidden">Logout</span>
+                                    <LogOut className="size-6 sm:hidden text-red-500 mt-2" />
                                 </button>
                             </form>
 
                             <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user.name}</span>
+                                <Avatar className="size-10">
+                                    <AvatarImage
+                                        src={session?.user?.image || ""}
+                                        alt={session?.user?.name || ""}
+                                    />
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
                             </Link>
                         </>
                     ) : (
