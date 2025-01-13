fd6b5673a66f86356cc571c139529a8bc190c073
jamie <xinjun@peachpay.app>
2025-01-13 18:09:52 +0800

    completed first stage of creating a startup pitch page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'easymde/dist/easymde.min.css'
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,57 +13,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-// const workSans = Work_Sans({
-//   src: [
-//     {
-//       path: "./fonts/WorkSans-Black.ttf",
-//       weight: "900",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-ExtraBold.ttf",
-//       weight: "800",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-Bold.ttf",
-//       weight: "700",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-SemiBold.ttf",
-//       weight: "600",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-Medium.ttf",
-//       weight: "500",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-Regular.ttf",
-//       weight: "400",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-Black.ttf",
-//       weight: "900",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-Thin.ttf",
-//       weight: "200",
-//       style: "normal",
-//     },
-//     {
-//       path: "./fonts/WorkSans-ExtraLight.ttf",
-//       weight: "100",
-//       style: "normal",
-//     },
-//   ],
-//   variable: "--font-work-sans",
-// });
 
 export const metadata: Metadata = {
   title: "YC Directory",
@@ -80,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster></Toaster>
       </body>
     </html>
   );
